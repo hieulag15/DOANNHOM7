25d57c82df3cbee8745e6671b555acff2c6a557e
BaoNun1205 <baole.120503@gmail.com>
2023-11-08 20:16:31 +0700

    xoas casi tao lao

--- DOAN/DATA/DBConnection.cs
+++ DOAN/DATA/DBConnection.cs
@@ -58,21 +58,6 @@ namespace DOAN.DATA
             da.Fill(ds); //Đưa dữ liệu truy vào Dataset
             return ds;
         }
-        public String autoGenerateID(string strSQL)
-        {
-            string result = "";
-            if (conn.State == ConnectionState.Open)//Nếu đang mở kết nối trước đó thì đóng lại
-                conn.Close();
-            conn.Open(); //Tạo một kết nối mới
-            comm.CommandText = strSQL;
-            comm.CommandType = CommandType.Text;
-            SqlDataReader dr = comm.ExecuteReader();
-            dr.Read();
-            result = dr[0].ToString();
-            dr.Close();
-            conn.Close();
-            return result;
-        }
 
         public string getButtons(string query, FlowLayoutPanel panelProduct, FlowLayoutPanel panelProductPay)
         {
