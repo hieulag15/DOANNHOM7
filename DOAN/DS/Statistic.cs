62804ec29d0ebe03d3c81d3b3dac904a8f6ebd23
thuytrang003 <truongnguyenthuytrang2003@gmail.com>
2023-11-14 15:46:00 +0700

    m

---
+++ DOAN/DS/Statistic.cs
@@ -0,0 +1,52 @@
+﻿using DOAN.DATA;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOAN.DS
+{
+    internal class Statistic
+    {
+        DBConnection db = null;
+        SqlCommand comm;
+        public Statistic()
+        {
+            db = new DBConnection();
+        }
+        public int tongKhachHang()
+        {
+            comm= new SqlCommand("select * from");
+            db = new DBConnection();
+            db.openConnection();
+            try
+            {
+                comm = new SqlCommand("proc_CreateAutoBillID", db.getSqlConn);
+                comm.CommandType = CommandType.StoredProcedure;
+
+                // Thêm tham số đầu vào nếu cần
+                // comm.Parameters.AddWithValue("@parameterName", parameterValue);
+
+                object result = comm.ExecuteScalar();
+
+                if (result != null)
+                {
+                    return result.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                db.closeConnection();
+            }
+
+            return "";
+        }
+    }
+}
