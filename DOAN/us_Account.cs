6f2c8ccea27b8ef0b6decfcb2bf1e022813154c7
hihieu04 <hihieu04@gmail.com>
2023-11-22 00:19:48 +0700

    sua ui, code account

--- DOAN/us_Account.cs
+++ DOAN/us_Account.cs
@@ -18,6 +18,7 @@ namespace DOAN
             InitializeComponent();
         }
 
+        bool addFlag;
         DataTable dtAccount = new DataTable();
         DataTable dtAccountActive = new DataTable();
         Account account = new Account();
@@ -31,7 +32,30 @@ namespace DOAN
             dgv_DanhSachHD.DataSource = dtAccount;
             setDataGridView();
         }
-
+        void not_fillingStatus()
+        {
+            txt_TaiKhoan.Enabled = false;
+            txt_MaNhanVien.Enabled = false;
+            txt_MatKhau.Enabled = false;
+            rbtn_Quanly.Enabled = false;
+            rb_HD.Enabled = false;
+            rb_KhongHD.Enabled = false;
+        }
+        void fillingStatus()
+        {
+            txt_TaiKhoan.Enabled = true;
+            txt_MaNhanVien.Enabled = true;
+            txt_MatKhau.Enabled = true;
+            rbtn_Quanly.Enabled = true;
+            rb_HD.Enabled = true;
+            rb_KhongHD.Enabled = true;
+        }
+        void initial_Status()
+        {
+            not_fillingStatus();
+            btn_Them.Enabled = true;
+            btn_Sua.Enabled = true;
+        }
         private void setDataGridView()
         {
             if (dgv_DanhSachHD != null)
@@ -54,20 +78,28 @@ namespace DOAN
         private void us_Account_Load(object sender, EventArgs e)
         {
             LoadAccount();
+            initial_Status();
         }
-
+        private void Refresh()
+        {
+            txt_TaiKhoan.Text = "";
+            txt_MaNhanVien.Text = "";
+            txt_MatKhau.Text = "";
+            rbtn_Quanly.Checked = false;
+            rb_HD.Checked = false;
+            rb_KhongHD.Checked = false;
+        }
         private void btn_Them_Click(object sender, EventArgs e)
         {
-            
-            try
-            {
-                account.addAccount(txt_TaiKhoan.Text.Trim(), txt_MatKhau.Text.Trim(), txt_MaNhanVien.Text.Trim(), role);
-                LoadAccount();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            addFlag = true;
+            btn_Them.Enabled = false;
+            btn_Luu.Enabled = true;
+            btn_Huy.Enabled = true;
+            btn_Sua.Enabled = false;
+
+            //Mở các ô cho phép điền thông tin\
+            fillingStatus();
+            Refresh();
         }
 
         private void rbtn_Quanly_CheckedChanged(object sender, EventArgs e)
@@ -93,8 +125,73 @@ namespace DOAN
 
         private void btn_Sua_Click(object sender, EventArgs e)
         {
-            account.updateAccount(txt_TaiKhoan.Text.Trim(), txt_MatKhau.Text.Trim());
+            //Trường hợp khi người dùng chưa chọn nhan vien cần sửa thông tin
+            if (txt_MaNhanVien.Text == "" || txt_MaNhanVien.Text == null)
+            {
+                MessageBox.Show("Vui lòng chọn nhân viên cần chỉnh sửa thông tin", "Lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+
+            btn_Them.Enabled = false;
+            btn_Luu.Enabled = true;
+            btn_Huy.Enabled = true;
+            btn_Sua.Enabled = true;
+
+            //Mở các ô cho phép điền thông tin\
+            fillingStatus();
+            txt_MaNhanVien.Enabled = false;
+        }
+
+        private void btn_Luu_Click(object sender, EventArgs e)
+        {
+            if (addFlag == true) //Trường hợp thêm nhan vien
+            {
+                try
+                {
+                    account.addAccount(txt_TaiKhoan.Text.Trim(), txt_MatKhau.Text.Trim(), txt_MaNhanVien.Text.Trim(), role);
+                    LoadAccount();
+                    Refresh();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Refresh();
+                }
+                addFlag = false;
+            }
+            else //Trường hợp người dùng nhấn update
+            {
+                try
+                {
+                    account.updateAccount(txt_TaiKhoan.Text.Trim(), txt_MatKhau.Text.Trim());
+                    LoadAccount();
+                    Refresh();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Refresh();
+                }
+            }
+            initial_Status();
+        }
+
+        private void btn_LamMoi_Click(object sender, EventArgs e)
+        {
+            Refresh();
+            initial_Status();
             LoadAccount();
+            txt_TimTheoTenTK.Text = "Theo tên tài khoản";
+            txt_TimTheoTen.Text = "Theo mã nhân viên";
+        }
+
+        private void btn_Huy_Click(object sender, EventArgs e)
+        {
+            Refresh();
+            initial_Status();
+            addFlag = false;
         }
     }
 }
